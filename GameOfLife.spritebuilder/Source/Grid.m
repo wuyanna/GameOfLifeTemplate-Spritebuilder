bb0023f65fac18c5e52095f146302e3a1490280c
Yanna Wu <wuyanna2009@gmail.com>
2015-01-25 17:00:00 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -64,7 +64,7 @@ static const int GRID_COLUMNS = 10;
     }
 }
 
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
     //get the x,y coordinates of the touch
     CGPoint touchLocation = [touch locationInNode:self];
